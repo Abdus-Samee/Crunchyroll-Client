402dd13445dcdd0581da0433c1697d91ba3de9b7
Abdus Samee <abdussamee16@gmail.com>
2022-01-26 00:10:19 +0600

    Added episode view in AnimeInfo component

--- src/components/AnimeInfo/AnimeInfo.js
+++ src/components/AnimeInfo/AnimeInfo.js
@@ -1,9 +1,14 @@
 import { useState, useEffect } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import Button from '@mui/material/Button'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemText from '@mui/material/ListItemText'
+import Divider from '@mui/material/Divider'
 
 const AnimeInfo = () => {
     const [info, setInfo] = useState({})
+    const [episodes, setEpisodes] = useState([])
     const navigate = useNavigate()
     const { animeId } = useParams()
 
@@ -11,6 +16,10 @@ const AnimeInfo = () => {
         fetch("http://localhost:9000/oracle/anime/"+animeId)
             .then(response => response.json())
             .then(data => setInfo(data))
+
+        fetch("http://localhost:9000/oracle/anime/episodes/"+animeId)
+            .then(response => response.json())
+            .then(data => setEpisodes(data))
     }, [])
 
     return(
@@ -18,7 +27,16 @@ const AnimeInfo = () => {
             <h1>{ info.TITLE }</h1>
             <h5>{ info.SYNOPSIS }</h5>
             <p>Released on : { new Date(info['Release Date'] ).toLocaleDateString() }</p>
-            <Button onClick={() => navigate('/anime')} size="small">Watch</Button>
+            <List component="nav" aria-label="mailbox folders">
+                {episodes.map(episode => (
+                    <>
+                        <ListItem button divider onClick={() => navigate('/anime')}>
+                            <ListItemText primary={'Episode ' + episode.EPISODE} />
+                        </ListItem>
+                        <Divider />
+                    </>
+                ))}
+            </List>
         </div>
     )
 }
