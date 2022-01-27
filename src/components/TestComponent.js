9f8d90fc09b5126a8a360725272a91f1f614a6ea
Abdus Samee <abdussamee16@gmail.com>
2022-01-27 18:58:08 +0600

    View for multiple images in manga

--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -1,7 +1,19 @@
+import { useState, useEffect } from 'react'
+
 import './imageStyles.css'
 
 const TestComponent = () => {
+    const [chapters, setChapters] = useState([])
     const divs = ['/images/one.jpg', '/images/two.jpg', '/images/four.jpg', '/images/five.jpg', '/images/six.png']
+
+    useEffect(() => {
+        fetch('http://localhost:9000/oracle/manga/chapters/1')
+            .then(response => response.json())
+            .then(data => {
+                setChapters(data)
+                console.log(data)
+            })
+    }, [])
 
     return(
         <div>
@@ -10,6 +22,10 @@ const TestComponent = () => {
                 <div styles={{ display: 'flex', flexDirection: 'column'}}>
                     {divs.map((div, index) => (
                         <img className="image" src={div} />
+                    ))}
+                    <h1>Server images...</h1>
+                    {chapters.map((chapter, index) => (
+                        <img className="image" src={'http://localhost:9000/oracle/manga/image/1/' + chapter.CHAPTER} />
                     ))}
                 </div>
             </div>
